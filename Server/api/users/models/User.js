d13a6610b98ee91e3b71d0e1dbb5dbbabd6bba5d
Thourum <samoasvanyi@gmail.com>
2016-06-24 10:48:00 +0200

    Password and Salt generation
    Generating salt and encrypt the password using the crypto (should switch to bcrypt) .
    Password is then encoded in bas64 and stored

--- Server/api/users/models/User.js
+++ Server/api/users/models/User.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -40,5 +41,112 @@ const UserModel = new Schema({
 		required: true
 	}
 });
+
+UserModel
+  .path('password')
+  .validate(function(password) {
+    return password.length;
+  }, 'Password cannot be blank');
+
+var validatePresenceOf = function(value) {
+  return value && value.length;
+};
+
+UserModel
+	.pre('save', function(next) {
+    // Handle new/update passwords
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    if (!validatePresenceOf(this.password)) {
+      return next(new Error('Invalid password'));
+    }
+
+    // Make salt with a callback
+    this.makeSalt((saltErr, salt) => {
+      if (saltErr) {
+        return next(saltErr);
+      }
+      this.salt = salt;
+      this.encryptPassword(this.password, (encryptErr, hashedPassword) => {
+        if (encryptErr) {
+          return next(encryptErr);
+        }
+        this.password = hashedPassword;
+        next();
+      });
+    });
+  });
+
+
+UserModel.methods = {
+
+/**
+   * Make salt
+   *
+   * @param {Number} byteSize Optional salt byte size, default to 16
+   * @param {Function} callback
+   * @return {String}
+   * @api public
+   */
+  makeSalt(byteSize, callback) {
+    var defaultByteSize = 16;
+
+    if (typeof arguments[0] === 'function') {
+      callback = arguments[0];
+      byteSize = defaultByteSize;
+    } else if (typeof arguments[1] === 'function') {
+      callback = arguments[1];
+    }
+
+    if (!byteSize) {
+      byteSize = defaultByteSize;
+    }
+
+    if (!callback) {
+      return crypto.randomBytes(byteSize).toString('base64');
+    }
+
+    return crypto.randomBytes(byteSize, (err, salt) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, salt.toString('base64'));
+      }
+    });
+  },
+
+  /**
+   * Encrypt password
+   *
+   * @param {String} password
+   * @param {Function} callback
+   * @return {String}
+   * @api public
+   */
+  encryptPassword(password, callback) {
+    if (!password || !this.salt) {
+      return null;
+    }
+
+    var defaultIterations = 10000;
+    var defaultKeyLength = 64;
+    var salt = new Buffer(this.salt, 'base64');
+
+    if (!callback) {
+      return crypto.pbkdf2Sync(password, salt, defaultIterations, defaultKeyLength)
+                   .toString('base64');
+    }
+
+    return crypto.pbkdf2(password, salt, defaultIterations, defaultKeyLength, (err, key) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, key.toString('base64'));
+      }
+    });
+  }
+};
 
 module.exports = mongoose.model('User', UserModel, 'users');
