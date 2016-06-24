ab10b0e67d92032e67327f5ce1dbd2d69609e31a
vagrant <joeRob2468@yahoo.com>
2016-06-24 16:07:52 +0000

    Adjusted User Password Field

--- Server/api/users/schemas/createUser.js
+++ Server/api/users/schemas/createUser.js
@@ -5,7 +5,7 @@ const Joi = require('joi');
 const createUserSchema = Joi.object({
 	username: Joi.string().alphanum().min(5).max(40).required(),
 	email: Joi.string().email().required(),
-	password: Joi.string().alphanum().min(6).max(30).required()
+	password: Joi.string().min(6).required()
 });
 
 module.exports = createUserSchema;
