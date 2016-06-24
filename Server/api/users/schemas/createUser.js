5656cb2a8569fa89391146a395e1327bec1e0c7e
Thourum <samoasvanyi@gmail.com>
2016-06-24 10:48:39 +0200

    Added Password to form

--- Server/api/users/schemas/createUser.js
+++ Server/api/users/schemas/createUser.js
@@ -4,7 +4,8 @@ const Joi = require('joi');
 
 const createUserSchema = Joi.object({
 	username: Joi.string().alphanum().min(5).max(40).required(),
-	email: Joi.string().email().required()
+	email: Joi.string().email().required(),
+	password: Joi.string().alphanum().min(6).max(30).required()
 });
 
 module.exports = createUserSchema;
