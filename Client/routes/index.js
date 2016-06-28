f428319a7876dcda6c574a5afd0725a815857af1
John Boyer <joeRob2468@yahoo.com>
2016-06-28 16:01:32 +0000

    Removed Riot.JS, refactored frontend

--- Client/routes/index.js
+++ Client/routes/index.js
@@ -7,6 +7,24 @@ router.get('/', function(req, res, next)
   res.render('pages/index');
 });
 
+<<<<<<< HEAD
+router.get('/login', function(req, res, next)
+{
+  res.render('pages/login');
+});
+
+router.get('/register', function(req, res, next)
+{
+  res.render('pages/register');
+});
+
+router.get('/profile', function(req, res, next)
+{
+  // temporarily redirect to login for now.
+  res.redirect('/login');
+})
+
+=======
 router.get('/register', function(req, res, next){
 	res.render('pages/register')
 });
@@ -15,5 +33,6 @@ router.get('/login', function(req, res, next){
 	res.render('pages/index')
 });
 
+>>>>>>> e44839d07a2f8b3e8839de3f11e8f01c24994d17
 
 module.exports = router;
