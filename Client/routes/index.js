9c08bdfcda9bf841363734a6e9d356a1234bda81
John Boyer <joeRob2468@yahoo.com>
2016-06-28 16:10:36 +0000

    Fixed merge conflicts

--- Client/routes/index.js
+++ Client/routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next)
   res.render('pages/index');
 });
 
-<<<<<<< HEAD
 router.get('/login', function(req, res, next)
 {
   res.render('pages/login');
@@ -22,17 +21,6 @@ router.get('/profile', function(req, res, next)
 {
   // temporarily redirect to login for now.
   res.redirect('/login');
-})
-
-=======
-router.get('/register', function(req, res, next){
-	res.render('pages/register')
 });
-
-router.get('/login', function(req, res, next){
-	res.render('pages/index')
-});
-
->>>>>>> e44839d07a2f8b3e8839de3f11e8f01c24994d17
 
 module.exports = router;
