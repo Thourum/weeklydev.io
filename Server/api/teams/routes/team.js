4623130d4a899c06fd931709a814ff269e219882
Thourum <samoasvanyi@gmail.com>
2016-07-04 17:46:22 +0200

    Add multiple people in roles
    Changed to use array instead of just a sting and now more then 1 person can be assigned the same role

--- Server/api/teams/routes/team.js
+++ Server/api/teams/routes/team.js
@@ -3,6 +3,10 @@
 const Boom = require('boom');
 const Team = require('../models/Team');
 const User = require('../../users/models/User');
+const teamSchema = require('../schemas/teamSchema');
+const Functions = require('../util/teamFunctions');
+const getRoles = Functions.getRoles;
+const arrayChecker = Functions.arrayChecker;
 
 module.exports = [{
   method: 'GET',
@@ -20,61 +24,75 @@ module.exports = [{
   path: '/teams',
   config: {
     // Validate the payload against the Joi schema
+    validate: {
+      payload: teamSchema
+    },
     // pre: [{
     //   method: 
     // }],
-
-    validate: {
-      // payload: surveySchema
-    },
     auth: 'jwt'
   },
   handler: (req, res) => {
     let team = new Team();
-    team.manager.push({user: req.payload.manager, role: req.payload.manager_role});
-    team.frontend.push({user: req.payload.frontend, role: req.payload.frontend_role});
-    team.frontend.push({user: req.payload.backend, role: req.payload.backend_role});
-    team.save((err, team) => {
-      if (err) {
-        console.log(err);
-        res(err);
+    let p = req.payload;
+    console.log(p);
+    if (!arrayChecker([p.manager , p.backend, p.frontend], [p.manager_role, p.backend_role, p.frontend_role])) {
+      res(Boom.badRequest('arrays of "role" and "*_role" must be the same length'));
+      return;
+    }else {
+      team.owner = req.Token.id;
+      team.manager = getRoles(p.manager, p.manager_role);
+      team.backend = getRoles(p.backend, p.backend_role);
+      team.frontend = getRoles(p.frontend, p.frontend_role);
+      team.save((err, team) => {
+        if (err) {
+          console.log(err);
+          res(err);
+        }else {
+          res({msg: 'created', id: team.id});
+        }
+      });
+    }
+  }
+}, {
+  method: 'PUT',
+  path: '/teams/{id}',
+  config: {
+    // Validate the payload against the Joi schema
+    // pre: [{
+    //   method: 
+    // }],
+
+    validate: {
+      payload: teamSchema
+    },
+    auth: 'jwt'
+  },
+  handler: (req, res) => {
+    Team.findById(req.params.id, (err, team) => {
+      if (req.Token.id !== team.owner) {
+        res(Boom.unauthorized('only team owner can update team details'));
       }else {
-        res({msg: 'created', id: team.id});
+        if (req.payload.owner) {
+          team.owner = req.payload.owner;
+        }
+        if (req.payload.manager && req.payload.manager_role) {}
+        team.manager.push({user: req.payload.manager, role: req.payload.manager_role});
+        team.frontend.push({user: req.payload.frontend, role: req.payload.frontend_role});
+        team.backend.push({user: req.payload.backend, role: req.payload.backend_role});
       }
     });
   }
 }, {
-  method: 'PUT',
-  path: '/teams',
-  config: {
-    // Validate the payload against the Joi schema
-    // pre: [{
-    //   method: 
-    // }],
-
-    validate: {
-      // payload: surveySchema
-    },
-    auth: 'jwt'
-  },
-  handler: (req, res) => {
-    res();
-  }
-}, {
   method: 'DELETE',
-  path: '/teams',
+  path: '/teams/{id}',
   config: {
     // Validate the payload against the Joi schema
-    // pre: [{
-    //   method: 
-    // }],
-
-    validate: {
-      // payload: surveySchema
-    },
     auth: 'jwt'
   },
   handler: (req, res) => {
-    res();
+    Team.findByIdAndRemove(req.params.id, (err, team) => {
+      res({msg: 'succes', team: team});
+    });
   }
 }];
