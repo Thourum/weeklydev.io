6df6f51866da9a1200ffd9d8f318963d3faee424
Thourum <samoasvanyi@gmail.com>
2016-07-08 14:25:38 +0200

    Add error code update

--- Server/config/errorCodes.js
+++ Server/config/errorCodes.js
@@ -13,9 +13,14 @@
 // 	Submissions 	: 	7xx
 'use strict';
 const Boom = require('boom');
+function applyErroCode (error) {
+  error.output.payload.errorCode = error.data.errorCode;
+  return error;
+}
 module.exports = {
-  userInTeam: Boom.create(400, 'User already on team', { errorCode: 500 }),
-  maxUsersInRole: Boom.create(400, 'Maximum users in role reached', { errorCode: 501 }),
-  maxRequestsReached: Boom.create(400, 'Maximum requests reached', { errorCode: 505 }),
-  alreadyRequested: Boom.create(400, 'You already requested to join this team', { errorCode: 506})
+  notOwner: applyErroCode(Boom.create(401, 'You are not owner of this team', { errorCode: 500})),
+  userInTeam: applyErroCode(Boom.create(400, 'User already on team', { errorCode: 501 })),
+  maxUsersInRole: applyErroCode(Boom.create(400, 'Maximum users in role reached', { errorCode: 502 })),
+  maxRequestsReached: applyErroCode(Boom.create(400, 'Maximum requests reached', { errorCode: 505 })),
+  alreadyRequested: applyErroCode(Boom.create(400, 'You already requested to join this team', { errorCode: 506}))
 };
